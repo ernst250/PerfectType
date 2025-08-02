36020a5a6189a2753d265236a2a3463a1b7d3dd7
Ernst <ernstsaincyr@gmail.com>
2025-08-02 00:37:42 -0400

    Supression d'un block de codes mis en commentaire

--- scripts/main.js
+++ scripts/main.js
@@ -10,30 +10,6 @@ lancerJeu();
     initAddEventListenerPopup();
 });
 
-// // recuperation da la zone de saisi
-// let inputEcriture = document.getElementById("inputEcriture");
-// console.log(inputEcriture);
-
-// //recuperation du bouton valider
-// let btnvaliderMot = document.getElementById("btnValiderMot");
-// console.log(btnvaliderMot);
-
-// // recuperer l'endoit ou le mot propose va afficher
-// let divZoneProposition = document.querySelector(".zoneProposition");
-// console.log(divZoneProposition);
-
-// // Recuperer l'endoit du score
-// let spanScore = document.querySelectorAll(".zoneScore span");
-// console.log(spanScore);
-
-// // recuperer tous les boutons radio
-// let listeBtnRadio = document.querySelectorAll(".optionSource input");
-// console.log(listeBtnRadio);
-
-
-// ajou nouvel element
-// let image = document.getElementById("petit");
-// image.src = "ombre.jpg"
 
 
 
